6a28964b99681ba0df8bdbb239b64e8fa4e4e3f5
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-06-12 00:56:30 -0400

    Initial commit for  assignment 2

---
+++ Client_side_web_development/Assignments/second_assignment/scripts/script.js
@@ -0,0 +1,9 @@
+// Image files in array
+let images = ["assets/images/heritagesite.jpg", "assets/images/bungyjump.jpg", "assets/images/forest.jpg", "assets/images/gumba.jpg",
+"assets/images/Lakes-Of-Nepal.webp","assets/images/mountain.jpg","assets/images/Paragliding.jpg", "assets/images/rafting.jpg", "assets/images/raftingNepal.jpeg",
+"assets/images/Rock-Climbing.jpg", "assets/images/templeNepal.jpg"];
+
+// Image counter
+let imageCount = images.length;
+
+//
