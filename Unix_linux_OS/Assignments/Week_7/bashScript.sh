40d1646eeab3b017ec2ca23f4876757501c02016
UjjwalPoudel010 <upoudel1@my.centennialcollege.ca>
2023-06-20 23:53:53 -0400

    Question no.1 solution

---
+++ Unix_linux_OS/Assignments/Week_7/bashScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo
+
+echo "The total number of arguments in the script are: $#"
+echo "The PID of the currently running script is: $$"
+echo "The name of the script is: $0"
